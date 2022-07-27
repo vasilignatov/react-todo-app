d9cacc7742b6f4b0594e007e0930f5b3e8c6dbda
Vasil <vasilignatow@gmail.com>
2022-07-27 14:14:56 +0300

    Add schema example

--- src/index.js
+++ src/index.js
@@ -2,15 +2,19 @@ const path = require('path');
 const express = require('express');
 const initHandlebars = require('./config/handlebars');
 
+const routes = require('./routes');
+
 const app = express();
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 // parse data from formdata
 
 initHandlebars(app);
 // init template engine
 
+app.use(express.static(path.resolve(__dirname, './public')));
+
+app.use(routes);
 
 
-
-app.listen(5000, console.log.bind(console, 'App is running on port 5000'));
+app.listen(5001, console.log.bind(console, `App is running on port http://localhost:5001`));

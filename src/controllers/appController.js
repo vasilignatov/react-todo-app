3b6655f059379a9fb6a544c0a587018346a5849e
Vasil <vasilignatow@gmail.com>
2022-08-02 19:46:18 +0300

    create template update

--- src/controllers/appController.js
+++ src/controllers/appController.js
@@ -4,20 +4,28 @@ const taskService = require('../services/taskService');
 
 const renderApp = async (req, res) => {
     let tasks = await taskService.getAll();
-    
-    res.render('app', { 
-        opened: tasks.opened, 
-        closed: tasks.closed, 
-        'in-progress': tasks['in-progress'] 
+
+    res.render('app', {
+        opened: tasks.opened,
+        closed: tasks.closed,
+        'in-progress': tasks['in-progress']
     });
+}
+
+const createTask = async (req, res) => {
+    let task = req.body;
+    console.log(task);
 }
 
 const renderCreate = (req, res) => {
     res.render('create');
-}   
+}
+
+
 
 router.get('/', renderApp);
 router.get('/create', renderCreate);
+router.post('/create', createTask);
 
 
 module.exports = router;

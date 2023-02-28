fb60669f789560d9be3e02d869a7e4e6172593aa
vasilignatov <76693214+vasilignatov@users.noreply.github.com>
2023-02-28 17:31:23 +0200

    Create Main component

--- spa/src/App.js
+++ spa/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/header/Header.js';
+import Main from './components/main/Main.js';
 
 function App() {
   return (
     <div class="app">
       <Header />
+      <Main />
     </div>
   );
 }
--- spa/src/components/main/Main.js
+++ spa/src/components/main/Main.js
@@ -1,3 +1,32 @@
 export default function Main() {
-    
+
+
+    return (
+        <main class="app__main">
+            <div class="todo__input">
+                <label for="create-todo">Todo: </label>
+                <input type="text" id="create-todo" name="create-todo" />
+                <button>Add</button>
+            </div>
+
+            <div class="todo__list">
+
+                <ul>
+
+                    <li class="todo">
+                        <input class="todo__isDone" type="checkbox" name="isDone" id="isDone" />
+                        <p class="todo__text">TASK TEXT</p>
+                        <button class="todo__btn">🗑</button>
+                    </li>
+
+                    {/*} <li class="todo">
+                    <input class="todo__isDone" type="checkbox" name="isDone" id="isDone">
+                    <p class="todo__text isDone">TASK TEXT</p>
+                    <button class="todo__btn">🗑</button>
+                    </li> */}
+                </ul>
+
+            </div>
+        </main>
+    )
 }

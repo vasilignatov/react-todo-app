053ec0b17482d4ef754ad2d7601c668a78db525f
vasilignatov <76693214+vasilignatov@users.noreply.github.com>
2023-03-01 21:16:13 +0200

    Create delete task handler

--- spa/src/components/main/Main.js
+++ spa/src/components/main/Main.js
@@ -1,35 +1,44 @@
 import { useState, useEffect } from 'react';
 import TodoItem from '../todoItem/TodoItem.js'
-
-const API_URL = 'http://localhost:3040/todos/';
+import { getTodos, createTodo, deleteTodo, updateTodo } from '../../services/todoService.js'
 
 export default function Main() {
-
     const [todos, setTodos] = useState([]);
-    
-    // useEffect(() => {
-    // }, [])
-    
-    fetch(API_URL)
-        .then(response => response.json())
-        .then(todos => setTodos(todos))
-        .catch(err => console.error(err));
-
     console.log(todos);
-    
+    useEffect(() => {
+        getTodos().then(data => setTodos(data));
+    }, []);
+
+    const onCreate = async (e) => {
+        const input = e.target.parentElement.querySelector('input');
+
+        if (input.value == '') return;
+
+        await createTodo({ text: input.value })
+        const newState = await getTodos();
+        setTodos(newState);
+        input.value = '';
+    }
+
     return (
         <main className="app__main">
             <div className="todo__input">
                 <label htmlFor="create-todo">Todo: </label>
                 <input type="text" id="create-todo" name="create-todo" />
-                <button>Add</button>
+                <button onClick={onCreate}>Add</button>
             </div>
 
             <div className="todo__list">
                 <ul>
                     {todos.map(todo => <TodoItem todo={todo} />)}
+                    {todos.map(todo =>
+                        <TodoItem
+                            key={todo.id}
+                            todo={todo}
+                            onDelete={onDelete}
+                        />)}
                 </ul>
             </div>
         </main>
-    )
+    );
 }

e539a5df21728622060b3d07dfed38b2b62c7576
vasilignatov <76693214+vasilignatov@users.noreply.github.com>
2023-03-01 21:14:19 +0200

    Added cors middleware

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,7 @@
 const PORT = 3040;
 
 const express = require('express');
+const cors = require('cors');
 const uniqid = require('uniqid');
 const data = require('./data.json');
 
@@ -8,8 +9,9 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
 
-console.log(data);
+// CRUD
 
 app.get('/todos', (req, res) => {
     try {
@@ -21,7 +23,6 @@ app.get('/todos', (req, res) => {
         })
     }
 });
-
 
 app.post('/todos', async (req, res) => {
     const { text } = req.body;
@@ -45,13 +46,28 @@ app.post('/todos', async (req, res) => {
 
 app.delete('/todos/:id', (req, res) => {
     const id = req.params.id;
+    const todoIndex = data.findIndex(x => x.id == id)
+    data.splice(todoIndex, 1);
 
+    res.status(204).json({message: 'Resource deleted successfully!'})
 });
+
+app.put('/todos/:id', (req, res) => {
+    const id = req.params.id;
+    const updatedTodo = req.body;
+
+    const todoIndex = data.findIndex(x => x.id == id);
+    data[todoIndex] = updatedTodo;
+
+    res.status(200).json({message: 'Resource updated successfully'})
+});
+
+
 
 app.get('*', (req, res) => {
     res.status(404).json({
         message: 'This resource doesn\'t exist!'
     });
-}) 
+})
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
